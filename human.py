1192c10555af3c69f13c953a23306a9c319cb989
Vamsi Spandan <vamsispandan@gmail.com>
2016-12-06 07:06:59 +0100

    part2 finished without cleaning up

---
+++ human.py
@@ -0,0 +1,9 @@
+
+# a file describing human anatomy
+
+str_name = 'vamsi'
+num_eyes = 2
+num_ears = 2
+num_nose = 1
+num_mouth = 1
+num_hands = 2
---
+++ my_name_is_so_big.py
@@ -0,0 +1,5 @@
+# My name is so big and also i have long names
+
+lang_1 = 'Fortran'
+lang_2 = 'C or C++'
+lang_3 = 'Java'
